f9db0a1ec3fb7d69225ecca6d8a1928b7c66e19e
jj-devhub <Jaid.jashim@infinitibit.com>
2025-09-07 07:23:51 +0600

    test: demonstrate pre-commit hook quality gate enforcement

--- test_precommit.py
+++ test_precommit.py
@@ -12,5 +12,11 @@ def another_function( a,b,c ):
     result=a+b+c
     return result
 
+# Add some code that will trigger flake8 errors
+def bad_function():
+    unused_variable = "this will trigger F841"
+    import json  # This will trigger E402 (import not at top)
+    return "test"
+
 if __name__=="__main__":
     print("Testing pre-commit hooks")
